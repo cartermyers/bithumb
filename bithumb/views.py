351a72d41017cccf0c1f29b180b052a3071f8bfb
Carter Myers <cartermyers123@gmail.com>
2018-03-11 12:29:18 -0600

    Html files (#2)
    * Add Juno's files
    
    * Update index from Juno
    
    * Update index page with django styling
    
    * Change static file references for local storage on index
    
    * Add working urls to all references (on index and profile)
    
    * Change html titles

--- bithumb/views.py
+++ bithumb/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-
     return render(request, 'bithumb/index.html')
+
+def about(request):
+    return render(request, 'bithumb/about_us.html')
