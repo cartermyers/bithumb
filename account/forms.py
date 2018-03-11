20a4a06e4b6264c0d2389e840be8bc9eaf2ff72c
Carter Myers <cartermyers123@gmail.com>
2018-03-11 16:38:32 -0600

    Account (#4)
    * Create signup form and logout
    
    * Add hashed passwords to signup
    
    * Add login form with functionality
    
    * Add signup and login functionality on main page
    
    * Add label for keep me logged in
    
    * Add responsive errors to sign up and log in
    
    * Add dependency from user to bank account
    
    * Alter fields for no underscores

---
+++ account/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth import password_validation
+
+from . import models
+
+class SignupForm(forms.ModelForm):
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Verify Password'}))
+
+    class Meta:
+        model = models.User
+        fields = ['username', 'password']
+
+    def clean(self):
+        #first, clean using the superclass
+        cleaned_data = super(SignupForm, self).clean()
+
+        #make sure the password is valid:
+        password = cleaned_data['password']
+        confirm = cleaned_data['confirm_password']
+
+        password_validation.validate_password(password)
+
+        if password != confirm:
+            self.add_error(None, "Passwords do not match.")
+
+#there is no admin form because they should be registered by developers
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    keep_me_logged_in = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'id': 'show_label'}))
--- account/views.py
+++ account/views.py
@@ -1,11 +1,80 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.hashers import make_password
 
-def signup(request):
-    return render(request, 'account/signup.html')
+from . import forms, models
 
-def profile(request, user_id):
-    #do the query here
-    return render(request, 'account/profile.html')
+def signup_view(request):
+
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('account:myprofile'))
+
+    login_form = forms.LoginForm()
+
+    if request.method == 'POST':
+        signup_form = forms.SignupForm(request.POST)
+
+        if signup_form.is_valid():
+            #save the new object
+            new_user = signup_form.save(commit=False)
+            new_user.set_password(make_password(signup_form.cleaned_data['password']))
+            new_user.save()
+
+            #login the new user
+            login(request, new_user)
+
+            #redirect to profile
+            return HttpResponseRedirect(reverse('account:myprofile'))
+    else:
+        signup_form = forms.SignupForm()
+
+    return render(request, 'bithumb/index.html', {'signup_form': signup_form, 'login_form': login_form})
+
+def login_view(request):
+
+    # this holds where the users should be redirected to
+    redirect = request.GET.get('next', reverse('index'))
+
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(redirect)
+
+    signup_form = forms.LoginForm()
+
+    if request.method == 'POST':
+        login_form = forms.LoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = authenticate(request, username=login_form.cleaned_data['username'], password=login_form.cleaned_data['password'])
+
+            # if user is not None, then there are no errors
+            if user:
+                login(request, user)
+                if login_form.cleaned_data['keep_me_logged_in']:
+                    request.session.set_expiry(60 * 60 * 24 * 10) # set expiry for 10 days
+                #else, uses the default expiry at browser close
+
+                return HttpResponseRedirect(redirect)
+            else:
+                login_form.add_error(None, 'Those are invalid credentials. Please try again.')
+    else:
+        login_form = forms.LoginForm()
+
+    return render(request, 'bithumb/index.html', {'signup_form': signup_form, 'login_form': login_form})
+
+
+def profile(request, user_id=None):
+    if user_id == None and request.user.is_authenticated:
+        user_id = request.user.pk
+
+    user = get_object_or_404(models.User, pk=user_id)
+
+    return render(request, 'account/profile.html', {'user': user})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
