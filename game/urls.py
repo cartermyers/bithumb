50cb6620ec59d78381fbb4aac995f7a33e6296e9
Carter Myers <cartermyers123@gmail.com>
2018-03-16 12:02:06 -0600

    Reference all new files except for images and sound

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.play_game, name='play_game'),
+    url(r'^game$', views.game, name='game'),
     url(r'^scoreboard/$', views.scoreboard, name='scoreboard'),
 ]
--- game/views.py
+++ game/views.py
@@ -2,6 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+#from django.auth.contrib.decorators import login_required
+
+#@login_required
+#this is the actual game used for the iframe
+def game(request):
+    return render(request, 'game/game.html')
+
+#@login_required
+#this is the actual page where players should be directed
+def play_game(request):
+    return render(request, 'game/index.html')
 
 def scoreboard(request):
     return render(request, 'game/scoreboard.html')
