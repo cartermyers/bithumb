e9f3b607fdf1cd7e714bdcfc880f5778f4a52b76
cartermyers <cartermyers123@gmail.com>
2018-03-17 11:23:49 -0600

    Add proper query for highscores

--- game/views.py
+++ game/views.py
@@ -3,5 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from account.models import User
+
 def scoreboard(request):
-    return render(request, 'game/scoreboard.html')
+    #get top 10 users by high score
+    top_users = User.objects.order_by('highscore')[:10]
+    return render(request, 'game/scoreboard.html', {'top_users': top_users})
