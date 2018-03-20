b1ab2cd4e3fd2bf3b7d2de7dfa09993a3f65d1da
Carter Myers <cartermyers123@gmail.com>
2018-03-20 14:30:31 -0600

    Remove debugging command

--- bitcoin_price_api/update_price.py
+++ bitcoin_price_api/update_price.py
@@ -4,13 +4,14 @@
 from django.conf import settings
 from bithumb import settings as my_settings
 
-if not settings.configured:
-    settings.configure(my_settings)
-
 import time
 from .exchanges import CoinDesk
 
 def update_price():
+    if not settings.configured:
+        settings.configure(my_settings)
+        print settings.PUSHER_CLUSTER
+
     subject = CoinDesk()
 
     #use a variable to see if the state has changed and we need to push to the client
@@ -20,7 +21,6 @@ def update_price():
         new_price = subject.get_state()['price']
         if new_price != old_state:
             old_state = new_price
-            print "Notifying {}".format(new_price)
             subject.notify()
 
         #only check about every 2 seconds
