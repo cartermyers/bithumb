f070ca722efdbdcd18b1a36905498c79ec8adac0
cartermyers <cartermyers123@gmail.com>
2018-03-20 10:53:03 -0600

    Try long running scripts with relative references

--- bitcoin_price_api/plot_graph.py
+++ bitcoin_price_api/plot_graph.py
@@ -7,7 +7,7 @@ import matplotlib
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
-from exchanges import CoinDesk
+from .exchanges import CoinDesk
 
 def plot_historical_price(start='2018-01-01', end=None):
 
@@ -24,23 +24,3 @@ def plot_historical_price(start='2018-01-01', end=None):
     plt.ylabel('USD Price')
 
     plt.plot(x_values, y_values)
-
-
-
-if __name__ == "__main__":
-    from datetime import datetime, timedelta
-
-    #first, set the size of the graph
-    plt.rcParams['figure.figsize'] = [13, 7.5]
-
-    #get the start of last week (in YYYY-MM-DD format; UTC timezone)
-    # that is, today - 7 days
-    last_week = datetime.now() - timedelta(days=7)
-    last_week = '{}-{:02d}-{:02d}'.format(last_week.year, last_week.month, last_week.day)
-
-    #plot the graph starting from last week
-    plot_historical_price(last_week)
-
-    #and save that to an image
-    plt.tight_layout()
-    plt.savefig('/home/shin202j/bithumb/static/img/weekly_prices.png', format='png')
