10877f397e9beea883548b660cf2f68cebb78db5
cartermyers <cartermyers123@gmail.com>
2018-03-22 10:45:59 -0600

    Fix set highscore bug for real

--- account/models.py
+++ account/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from decimal import Decimal
+
 from invest.models import BankAccount, BankAccountToCollectible
 
 class User(AbstractUser):
