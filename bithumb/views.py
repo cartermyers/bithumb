0a63956e1ca4a4b7ebd2585fc7206b054c6d3f17
Carter Myers <cartermyers123@gmail.com>
2018-03-22 10:04:22 -0600

    Headers (#14)
    * Move game to main index page
    
    * Add proper header for all html pages
    
    * Add profile links on forum pages

--- bithumb/views.py
+++ bithumb/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from account.forms import SignupForm, LoginForm
 
 def index(request):
-    return render(request, 'bithumb/index.html', {'signup_form': SignupForm(), 'login_form': LoginForm()})
+    total_in_game_currency = None
+    if request.user.is_authenticated:
+        total_in_game_currency = request.user.get_bank_account().get_in_game_currency()
+
+    return render(request, 'bithumb/index.html', {'signup_form': SignupForm(), 'login_form': LoginForm(), 'total_score': total_in_game_currency})
 
 def about(request):
     return render(request, 'bithumb/about_us.html')
